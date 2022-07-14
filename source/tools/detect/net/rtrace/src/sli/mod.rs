a07c9726ff52f29e400b1f8bb84cc93a9db4455e
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2022-07-14 16:49:09 +0800

    rtrace: handle sli event and get latency sli from bpf map
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- source/tools/detect/net/rtrace/src/sli/mod.rs
+++ source/tools/detect/net/rtrace/src/sli/mod.rs
@@ -3,6 +3,9 @@ mod event;
 use eutils_rs::proc::Snmp;
 mod sli;
 use anyhow::{bail, Result};
+use event::Event;
+use event::LatencyHist;
+use sli::Sli;
 use std::{thread, time};
 use structopt::StructOpt;
 
@@ -19,13 +22,14 @@ pub struct SliCommand {
 pub struct SliOutput {
     // retranmission metrics
     // retran = (RetransSegs－last RetransSegs) ／ (OutSegs－last OutSegs) * 100%
-    outsegs: u32,   // Tcp: OutSegs
-    retran: u32,    // Tcp: RetransSegs
+    outsegs: isize, // Tcp: OutSegs
+    retran: isize,  // Tcp: RetransSegs
 
     drop: u32,
 
-    latency: [u32;1000],
-    
+    latencyhist: LatencyHist,
+
+    events: Vec<Event>,
 }
 
 fn snmp_delta(old: &Snmp, new: &Snmp, key: (&str, &str)) -> Result<isize> {
@@ -42,20 +46,51 @@ fn snmp_delta(old: &Snmp, new: &Snmp, key: (&str, &str)) -> Result<isize> {
     bail!("failed to find key: {:?}", key)
 }
 
+fn latency_sli(sli: &mut Sli) -> Result<()> {
+    Ok(())
+}
+
 pub fn build_sli(opts: &SliCommand) -> Result<()> {
     let mut old_snmp = Snmp::from_file("/proc/net/snmp")?;
-    let sleep = time::Duration::from_secs(opts.period);
+    let delta_ns = opts.period * 1_000_000_000;
+    let mut sli = Sli::new(log::log_enabled!(log::Level::Debug))?;
+    let mut sli_output: SliOutput = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
+    let mut pre_ts = 0;
+
+    if opts.latency {
+        sli.attach_latency()?;
+    }
 
     loop {
-        thread::sleep(sleep);
+        if let Some(event) = sli.poll(std::time::Duration::from_millis(100))? {
+            // log::debug!("{}", event);
+            sli_output.events.push(event);
+        }
+
+        let cur_ts = eutils_rs::timestamp::current_monotime();
+        if cur_ts - pre_ts < delta_ns {
+            continue;
+        }
+
+        pre_ts = cur_ts;
+
         let new_snmp = Snmp::from_file("/proc/net/snmp")?;
 
         if opts.retran {
+            sli_output.outsegs = snmp_delta(&old_snmp, &new_snmp, ("Tcp:", "OutSegs"))?;
+            sli_output.retran = snmp_delta(&old_snmp, &new_snmp, ("Tcp:", "RetransSegs"))?;
+
             println!(
                 "OutSegs: {}, Retran: {}",
                 snmp_delta(&old_snmp, &new_snmp, ("Tcp:", "OutSegs"))?,
                 snmp_delta(&old_snmp, &new_snmp, ("Tcp:", "RetransSegs"))?,
             );
+        }
+
+        if opts.latency {
+            if let Some(x) = sli.lookup_and_update_latency_map()? {
+                sli_output.latencyhist = x;
+            }
         }
 
         old_snmp = new_snmp;
