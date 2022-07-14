a2b617eba8f4cbaef878243037e6c2f8554d0448
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2022-07-14 16:15:08 +0800

    rtrace: add build sli eBPF code script and import bindings and skel.
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- source/tools/detect/net/rtrace/src/main.rs
+++ source/tools/detect/net/rtrace/src/main.rs
@@ -29,6 +29,15 @@ mod abnormal_bindings;
 #[path = "abnormal/bpf/.output/tcp.skel.rs"]
 pub mod abnormaltcpskel;
 
+#[path = "sli/bpf/.output/bindings.rs"]
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+mod sli_bindings;
+#[path = "sli/bpf/.output/sli.skel.rs"]
+pub mod sliskel;
+
 mod abnormal;
 mod drop;
 mod latency;
