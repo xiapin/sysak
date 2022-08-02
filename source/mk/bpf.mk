ff2b610f1d2202f89eecc2ae4415e95a88b75ce2
Hailong Liu <liuhailong@linux.alibaba.com>
2022-08-02 11:06:30 +0800

    bpf.mk: remove "-static" for LDFLAGS
    The flag "-static" may be not compatible with other flags, such as "-lrt".
    So remove it.
    
    Signed-off-by: Hailong Liu <liuhailong@linux.alibaba.com>

--- source/mk/bpf.mk
+++ source/mk/bpf.mk
@@ -14,7 +14,7 @@ endif
 
 DEPEND := "prev{btf}"
 
-CFLAGS += $(EXTRA_CLFAGS) -g -O2 -Wall -static
+CFLAGS += $(EXTRA_CLFAGS) -g -O2 -Wall
 LDFLAGS += $(EXTRA_LDFLAGS)
 INCLUDES += $(EXTRA_INCLUDES) -I$(OBJPATH) -I$(SRC)/lib/internal/ebpf -I$(TARGET_PATH) -I$(OBJ_LIB_PATH) -I$(SRC)/lib/internal/ebpf/libbpf/include/uapi -I$(SRC)/lib/uapi/include
 
