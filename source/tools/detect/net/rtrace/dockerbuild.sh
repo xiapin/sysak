074b7421dbd0d04f081c6066ed836a302df95bb7
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2022-08-05 15:51:49 +0800

    rtrace: add docker shell to compile rtrace in docker environment
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

---
+++ source/tools/detect/net/rtrace/dockerbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# THIS FILE WORKS WITH RTRACE
+# prepare your docker environment
+# step 1. docker pull registry.cn-hangzhou.aliyuncs.com/alinux/lcc:latest
+# step 2. docker run --name bpf-build -itd -v $HOME:$HOME --net host registry.cn-hangzhou.aliyuncs.com/alinux/lcc:latest /sbin/init
+# step 3. docker exec -it bpf-build /bin/bash
+# step 4. curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# step 5. yum install gcc
+
+workpath=`pwd`
+docker exec -it bpf-build sh -c  "cd $workpath && source \"$HOME/.cargo/env\" && cargo build --release && cp $workpath/target/release/rtrace $workpath"
