261408e2d370ef111ed22ea2c83d71f4a1bc29ac
Hailong Liu <liuhailong@linux.alibaba.com>
2022-08-10 15:21:48 +0800

    irqoff: Take the shm as the summary mode recording
    Take share memory as the recording for summary-mode, so others can
    parse it easily.
    
    Signed-off-by: Hailong Liu <liuhailong@linux.alibaba.com>

--- source/tools/detect/sched/irqoff/irqoff.h
+++ source/tools/detect/sched/irqoff/irqoff.h
@@ -17,31 +17,9 @@ struct arg_info {
 	__u64 thresh;
 };
 
-struct event {
-	__u32 ret, pid, cpu;
-	__u64 delay, stamp;
-	char comm[TASK_COMM_LEN];
-};
-
 struct ksym {
 	long addr;
 	char *name;
 };
-
-struct max_sum {
-	__u64 value;
-	__u64 stamp;
-	int cpu, pid;
-	char comm[TASK_COMM_LEN];
-};
-
-struct summary {
-	unsigned long num;
-	__u64	total;
-	struct max_sum max;
-	int cpus[CPU_ARRY_LEN];
-	int jitter[CPU_ARRY_LEN];
-	char container[CPU_ARRY_LEN][CONID_LEN];
-};
-#endif /* __LLCSTAT_H */
+#endif /* __IRQOFF_H */
 
