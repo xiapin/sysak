c72a7142058c3f4baf5b08a218f310491aa48d28
Hailong Liu <liuhailong@linux.alibaba.com>
2022-08-10 15:29:54 +0800

    runqslower: Take the shm as the summary mode recording
    Take share memory as the recording for summary-mode, so others can
    parse it easily.
    
    Signed-off-by: Hailong Liu <liuhailong@linux.alibaba.com>

--- source/tools/detect/sched/runqslower/runqslower.h
+++ source/tools/detect/sched/runqslower/runqslower.h
@@ -5,7 +5,7 @@
 #define CPU_ARRY_LEN	4
 #define CONID_LEN	13
 
-struct event {
+struct rqevent {
 	char task[TASK_COMM_LEN];
 	char prev_task[TASK_COMM_LEN];
 	
@@ -14,22 +14,6 @@ struct event {
 	pid_t pid;
 	pid_t prev_pid;
 	int cpuid;
-};
-
-struct max_sum {
-	__u64 value;
-	__u64 stamp;
-	int cpu, pid;
-	char comm[TASK_COMM_LEN];
-};
-
-struct summary {
-	unsigned long num;
-	__u64	total;
-	struct max_sum max;
-	int cpus[CPU_ARRY_LEN];
-	int jitter[CPU_ARRY_LEN];
-	char container[CPU_ARRY_LEN][CONID_LEN];
 };
 
 struct args {
