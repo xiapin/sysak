67f0ecfbe8b2b073889777fd8e0009ca3a7e3faf
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2022-08-24 15:22:11 +0800

    rtrace: refactor drop module code
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- source/tools/detect/net/rtrace/src/drop/bpf/drop.h
+++ source/tools/detect/net/rtrace/src/drop/bpf/drop.h
@@ -1,100 +1,13 @@
 #ifndef __DROP_H
 #define __DROP_H
 
-#ifndef u8
-typedef unsigned char u8;
-#endif
+#include "common.h"
 
-#ifndef u16
-typedef unsigned short int u16;
-#endif
-
-#ifndef u32
-typedef unsigned int u32;
-#endif
-
-#ifndef u64
-typedef long long unsigned int u64;
-#endif
-
-enum{
-    ERR_OK = 0,
-    ERR_PROTOCOL_NOT_DETERMINED,
-    ERR_PROTOCOL_NOT_SUPPORT,
-};
-
-enum {
-    KFREE_SKB = 0,
-    TCP_DROP,
-    IPTABLES,
-};
-/**
- * struct addr_pair -
- * @saddr: be
- * @sport:
- * @daddr: be
- * @dport:
- */
-struct addr_pair
-{
-    u32 saddr;
-    u16 sport;
-    u16 dport;
-    u32 daddr;
-};
-
-struct filter
-{
-    struct addr_pair ap;
-};
-
-struct pid_info
-{
-    u32 pid;
-    u8 comm[16];
-};
-
-struct tcp_params
-{
-    u32 syn_qlen; // syn queue
-    u32 max_len;
-    u8 syncookies;
-    u32 acc_qlen; // accept queue
-};
-
-struct iptables_params
-{
-    u8 name[32];
-    u32 hook;
-};
-
-struct event
-{
-    int stackid;
-    u8 error;
-    u8 type;
-    struct pid_info pi;
-
-    struct addr_pair ap;
-    struct addr_pair skap;
-
-    u8 state; // tcp state
-
-    u16 sk_protocol;
-    u16 protocol;
-
-     union
-    {
-        struct tcp_params tp;
 #define IPTABLE_FILTER 1
 #define IPTABLE_MANGLE 2
 #define IPTABLE_RAW 3
 #define ARPTABLE_FILTER 4  
 #define IPTABLE_SECURITY 5
 #define IPTABLE_NAT 6
-        struct iptables_params ip;
-        
-    };
-};
 
 #endif
--- source/tools/detect/net/rtrace/src/drop/drop.rs
+++ source/tools/detect/net/rtrace/src/drop/drop.rs
@@ -1,185 +1,206 @@
-use crate::drop::event::Event;
-use crate::drop::bindings::*;
-use crate::drop::dropskel::*;
-use crate::perf::PerfBufferBuilder;
-use anyhow::{bail, Result};
-use crossbeam_channel;
-use crossbeam_channel::Receiver;
-use libbpf_rs::MapFlags;
-use once_cell::sync::Lazy;
-use std::ffi::CString;
-use std::sync::Mutex;
-use std::time::Duration;
+use crate::common::*;
+use crate::drop::skel::*;
+use crate::stack::Stack;
+use crate::utils::*;
+use crate::Command;
+use anyhow::Result;
+use byteorder::{NativeEndian, ReadBytesExt};
+use chrono::prelude::*;
+use eutils_rs::proc::{Kallsyms, Netstat};
+use eutils_rs::{net::ProtocolType, net::TcpState, proc::Snmp};
+use std::collections::HashMap;
+use std::fmt;
+use structopt::StructOpt;
 
-static GLOBAL_TX: Lazy<Mutex<Option<crossbeam_channel::Sender<(usize, Vec<u8>)>>>> =
-    Lazy::new(|| Mutex::new(None));
+#[derive(Debug, StructOpt)]
+pub struct DropCommand {
+    #[structopt(long, default_value = "tcp", help = "Network protocol type")]
+    proto: String,
+    #[structopt(long, help = "Process identifier of container")]
+    pid: Option<usize>,
+    #[structopt(long, help = "Local network address of traced sock")]
+    src: Option<String>,
+    #[structopt(long, help = "Remote network address of traced sock")]
+    dst: Option<String>,
 
-pub fn handle_event(_cpu: i32, data: &[u8]) {
-    let event = Vec::from(data);
-    GLOBAL_TX
-        .lock()
-        .unwrap()
-        .as_ref()
-        .unwrap()
-        .send((_cpu as usize, event))
-        .unwrap();
+    #[structopt(long, help = "Enable monitoring iptables modules")]
+    iptables: bool,
+    #[structopt(long, help = "Enable monitoring conntrack modules")]
+    conntrack: bool,
+    #[structopt(long, help = "Enable monitoring kfree_skb")]
+    kfree: bool,
+    #[structopt(long, help = "Enable monitoring tcp_drop")]
+    tcpdrop: bool,
+
+    #[structopt(
+        long,
+        default_value = "3",
+        help = "Period of display in seconds. 0 mean display immediately when event triggers"
+    )]
+    period: u64,
 }
 
-pub fn handle_lost_events(cpu: i32, count: u64) {
-    eprintln!("Lost {} events on CPU {}", count, cpu);
-}
+fn get_enabled_points(opts: &DropCommand) -> Result<Vec<&str>> {
+    let mut enabled = vec![];
 
-fn bump_memlock_rlimit() -> Result<()> {
-    let rlimit = libc::rlimit {
-        rlim_cur: 128 << 20,
-        rlim_max: 128 << 20,
-    };
-
-    if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) } != 0 {
-        bail!("Failed to increase rlimit");
+    if opts.conntrack {
+        enabled.push("__nf_conntrack_confirm");
+        enabled.push("__nf_conntrack_confirm_ret");
     }
 
-    Ok(())
-}
-
-fn open_load_skel<'a>(debug: bool, btf: &Option<String>) -> Result<DropSkel<'a>> {
-    let btf_cstring;
-    let mut btf_cstring_ptr = std::ptr::null();
-    if let Some(btf) = btf {
-        btf_cstring = CString::new(btf.clone())?;
-        btf_cstring_ptr = btf_cstring.as_ptr();
+    if opts.iptables {
+        if eutils_rs::KernelVersion::current()? >= eutils_rs::KernelVersion::try_from("4.10.0")? {
+            enabled.push("ipt_do_table");
+        } else {
+            enabled.push("ipt_do_table310");
+        }
+        enabled.push("ipt_do_table_ret");
     }
 
-    bump_memlock_rlimit()?;
-    let mut skel_builder = DropSkelBuilder::default();
-    skel_builder.obj_builder.debug(debug);
-    let mut open_opts = skel_builder.obj_builder.opts(std::ptr::null());
-    open_opts.btf_custom_path = btf_cstring_ptr;
-    let mut open_skel = skel_builder.open_opts(open_opts)?;
-    Ok(open_skel.load()?)
+    if opts.kfree {
+        if eutils_rs::KernelVersion::current()? >= eutils_rs::KernelVersion::try_from("5.10.0")? {
+            enabled.push("kfree_skb");
+        } else {
+            enabled.push("kfree_skb");
+        }
+    }
+
+    if opts.tcpdrop {
+        if eutils_rs::KernelVersion::current()? >= eutils_rs::KernelVersion::try_from("5.10.0")? {
+            enabled.push("kfree_skb");
+        } else {
+            enabled.push("tcp_drop");
+        }
+    }
+
+    Ok(enabled)
 }
 
-pub struct Drop<'a> {
-    skel: DropSkel<'a>,
-    rx: Option<Receiver<(usize, Vec<u8>)>>,
-    filter: filter,
+pub fn build_drop(opts: &DropCommand, debug: bool, btf: &Option<String>) -> Result<()> {
+    let kallsyms = Kallsyms::try_from("/proc/kallsyms")?;
+
+    // load, set filter and attach
+    let mut filter = Filter::new();
+    filter.set_ap(&opts.src, &opts.dst)?;
+
+    let mut skel = Skel::default();
+    let enabled = get_enabled_points(&opts)?;
+    skel.open_load_enabled(debug, btf, enabled)?;
+    skel.filter_map_update(0, unsafe {
+        std::mem::transmute::<commonbinding::filter, filter>(filter.filter())
+    })?;
+    skel.attach()?;
+
+    let mut drop = Drop::new(&opts.pid)?;
+    // poll data
+    let mut pre_ts = 0;
+    loop {
+        if let Some(data) = skel.poll(std::time::Duration::from_millis(100))? {
+            let event = Event::new(data);
+
+            let key = unsafe { std::mem::transmute::<u32, [u8; 4]>(event.stackid()) };
+            let stack = Stack::new(Vec::new());
+            match skel
+                .skel
+                .maps_mut()
+                .stackmap()
+                .lookup(&key, libbpf_rs::MapFlags::ANY)
+            {
+                Ok(stack) => {}
+                Err(e) => {}
+            }
+
+            drop.insert_event(event, stack);
+        }
+
+        let cur_ts = eutils_rs::timestamp::current_monotime();
+        if cur_ts - pre_ts > opts.period * 1_000_000_000 {
+            pre_ts = cur_ts;
+            drop.update()?;
+            println!("{}", drop);
+            drop.clear_events();
+        }
+    }
 }
 
-impl<'a> Drop<'a> {
-    pub fn new(debug: bool, btf: &Option<String>) -> Result<Drop<'a>> {
-        let skel = open_load_skel(debug, btf)?;
+pub struct Drop {
+    delta_snmp: DeltaSnmp,
+    delta_netstat: DeltaNetstat,
+    delta_dev: DeltaDev,
+    events: Vec<(Event, Stack)>,
+}
+
+impl Drop {
+    pub fn new(pid: &Option<usize>) -> Result<Drop> {
+        let mut snmp_path = "/proc/net/snmp".to_owned();
+        let mut netstat_path = "/proc/net/netstat".to_owned();
+        if let Some(x) = pid {
+            snmp_path = format!("/proc/{}/net/snmp", x);
+            netstat_path = format!("/proc/{}/net/netstat", x);
+        }
         Ok(Drop {
-            skel,
-            rx: None,
-            filter: unsafe { std::mem::MaybeUninit::zeroed().assume_init() },
+            delta_snmp: DeltaSnmp::new(&snmp_path)?,
+            delta_netstat: DeltaNetstat::new(&netstat_path)?,
+            delta_dev: DeltaDev::new()?,
+            events: Vec::default(),
         })
     }
 
-    pub fn poll(&mut self, timeout: Duration) -> Result<Option<Event>> {
-        if let Some(rx) = &self.rx {
-            let data_warp = rx.recv_timeout(timeout);
-            match data_warp {
-                Ok(data) => {
-                    let event = Event::new(data.1);
-                    return Ok(Some(event));
+    pub fn update(&mut self) -> Result<()> {
+        self.delta_snmp.update()?;
+        self.delta_netstat.update()?;
+        self.delta_dev.update()?;
+        Ok(())
+    }
+
+    pub fn insert_event(&mut self, event: Event, stack: Stack) {
+        self.events.push((event, stack));
+    }
+
+    pub fn clear_events(&mut self) {
+        self.events.clear();
+    }
+}
+
+impl fmt::Display for Drop {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Total {} packets drop", self.events.len())?;
+
+        for x in &self.events {
+            let event = &x.0;
+            writeln!(
+                f,
+                "{} SKB INFO: protocol: {} \t{} -> {}",
+                Utc::now().format("%Y-%m-%d %H:%M:%S"),
+                event.protocol(),
+                event.local(),
+                event.remote()
+            )?;
+            let skap = event.skap();
+            writeln!(
+                f,
+                "\t \t SOCK INFO: protocol: {} \t {} -> {}, \t state: {}",
+                event.sk_protocol(),
+                skap.0,
+                skap.1,
+                TcpState::from(event.state() as i32),
+            )?;
+
+            match event.event_type() {
+                EventType::DropIptables => {
+                    println!(
+                        "Table:{} Chain:{}",
+                        event.iptables_name(),
+                        event.iptables_chain_name()
+                    );
                 }
-                _ => {
-                    return Ok(None);
-                }
+                _ => {}
             }
         }
-        let (tx, rx) = crossbeam_channel::unbounded();
-        self.rx = Some(rx);
-        *GLOBAL_TX.lock().unwrap() = Some(tx);
-        let perf = PerfBufferBuilder::new(self.skel.maps_mut().events())
-            .sample_cb(handle_event)
-            .lost_cb(handle_lost_events)
-            .build()?;
-        std::thread::spawn(move || loop {
-            perf.poll(timeout).unwrap();
-        });
-        log::debug!("start successfully perf thread to receive event");
-        Ok(None)
-    }
 
-    pub fn set_filter_ap(&mut self, ap: addr_pair) {
-        self.filter.ap = ap;
-    }
-
-    pub fn update_filter(&mut self) -> Result<()> {
-        let mut key = vec![0; self.skel.maps().filter_map().key_size() as usize];
-        let key_ptr = &mut key[0] as *mut u8 as *mut i32;
-        unsafe {
-            *key_ptr = 0;
-        }
-        self.skel.maps_mut().filter_map().update(
-            &key,
-            unsafe {
-                std::slice::from_raw_parts(
-                    &self.filter as *const filter as *const u8,
-                    std::mem::size_of::<filter>(),
-                )
-            },
-            MapFlags::ANY,
-        )?;
-        Ok(())
-    }
-
-
-    pub fn get_stack(&mut self, stackid: i32) -> Result<Vec<u8>> {
-        let res = self.skel.maps_mut().stackmap().lookup(
-            unsafe { std::slice::from_raw_parts(&stackid as *const i32 as *const u8, 4) },
-            MapFlags::ANY,
-        )?;
-
-        match res {
-            Some(x) => Ok(x),
-            None => bail!("failed to get stack from stackid - {}", stackid),
-        }
-    }
-
-    pub fn attach_kfreeskb(&mut self) -> Result<()> {
-        if eutils_rs::KernelVersion::current()? >= eutils_rs::KernelVersion::try_from("5.10.0")? {
-            if self.skel.links.kfree_skb.is_none() {
-                self.skel.links.kfree_skb = Some(self.skel.progs_mut().kfree_skb().attach()?);
-            }
-        } else {
-            self.skel.links.kfree_skb = Some(self.skel.progs_mut().kfree_skb().attach()?);
-        }
-        Ok(())
-    }
-
-    pub fn attach_tcpdrop(&mut self) -> Result<()> {
-        if eutils_rs::KernelVersion::current()? >= eutils_rs::KernelVersion::try_from("5.10.0")? {
-            if self.skel.links.kfree_skb.is_none() {
-                self.skel.links.kfree_skb = Some(self.skel.progs_mut().kfree_skb().attach()?);
-            }
-        } else {
-            self.skel.links.tcp_drop = Some(self.skel.progs_mut().tcp_drop().attach()?);
-        }
-        Ok(())
-    }
-
-    pub fn attach_iptables(&mut self) -> Result<()> {
-        if eutils_rs::KernelVersion::current()? >= eutils_rs::KernelVersion::try_from("4.10.0")? {
-            self.skel.links.ipt_do_table = Some(self.skel.progs_mut().ipt_do_table().attach()?);
-        } else {
-            self.skel.links.ipt_do_table310 = Some(self.skel.progs_mut().ipt_do_table().attach()?);
-        }
-        self.skel.links.ipt_do_table_ret = Some(self.skel.progs_mut().ipt_do_table_ret().attach()?);
-
-        Ok(())
-    }
-
-    pub fn attach_conntrack(&mut self) -> Result<()> {
-        self.skel.links.__nf_conntrack_confirm =
-            Some(self.skel.progs_mut().__nf_conntrack_confirm().attach()?);
-        self.skel.links.__nf_conntrack_confirm_ret = Some(
-            self.skel
-                .progs_mut()
-                .__nf_conntrack_confirm_ret()
-                .attach()?,
-        );
-        Ok(())
+        writeln!(f, "Delta Snmp\n{}", self.delta_snmp)?;
+        writeln!(f, "Delta Netstat\n{}", self.delta_netstat)?;
+        writeln!(f, "Delta Device Status\n{}", self.delta_dev)?;
+        write!(f, "")
     }
 }
