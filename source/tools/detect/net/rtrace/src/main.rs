00b92e29590a8351fa1361b284b8826ce8588794
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2022-11-29 15:20:42 +0800

    rtrace: fix conflict
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- source/tools/detect/net/rtrace/src/main.rs
+++ source/tools/detect/net/rtrace/src/main.rs
@@ -32,26 +32,13 @@ pub struct Command {
 
 #[derive(Debug, StructOpt)]
 enum SubCommand {
-<<<<<<< HEAD
-    #[structopt(name = "abnormal", about = "Abnormal connection diagnosing")]
-    Abnormal(AbnormalCommand),
-=======
->>>>>>> 5d143e18600de5c4873b9ba1dc4e02fc282dae96
     #[structopt(name = "drop", about = "Packet drop diagnosing")]
     Drop(DropCommand),
     #[structopt(name = "latency", about = "Packet latency tracing")]
     Latency(LatencyCommand),
-<<<<<<< HEAD
-    // #[structopt(name = "latencylegacy", about = "Packet latency tracing(legacy version)")]
-    // LatencyLegacy(LatencyLegacyCommand),
-    // #[structopt(name = "sli", about = "Collection machine sli")]
-    // Sli(SliCommand),
-    #[structopt(name = "retran", about = "Packet retransmission tracing")]
-=======
     #[structopt(name = "delta", about = "Store or display delta information")]
     Delta(DeltaCommand),
     #[structopt(name = "retran", about = "Tracing retransmit")]
->>>>>>> 5d143e18600de5c4873b9ba1dc4e02fc282dae96
     Retran(RetranCommand),
 }
 
@@ -63,30 +50,15 @@ fn main() -> Result<()> {
     eutils_rs::helpers::bump_memlock_rlimit()?;
 
     match opts.subcommand {
-<<<<<<< HEAD
-        SubCommand::Abnormal(cmd) => {
-            build_abnormal(&cmd, opts.verbose, &opts.btf)?;
-        }
-=======
->>>>>>> 5d143e18600de5c4873b9ba1dc4e02fc282dae96
         SubCommand::Drop(cmd) => {
             run_drop(&cmd, opts.verbose, &btf);
         }
         SubCommand::Latency(cmd) => {
             run_latency(&cmd, opts.verbose, &btf);
         }
-<<<<<<< HEAD
-        // SubCommand::LatencyLegacy(cmd) => {
-        //     build_latency_legacy(&cmd, opts.verbose, &opts.btf)?;
-        // }
-        // SubCommand::Sli(cmd) => {
-        //     build_sli(&cmd)?;
-        // }
-=======
         SubCommand::Delta(cmd) => {
             run_delta(&cmd);
         }
->>>>>>> 5d143e18600de5c4873b9ba1dc4e02fc282dae96
         SubCommand::Retran(cmd) => {
             run_retran(&cmd, opts.verbose, &btf);
         }
