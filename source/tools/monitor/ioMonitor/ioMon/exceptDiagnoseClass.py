cf6aef8156f5e20908d78071e78640374e86f05b
guangshui.li <guangshui.li@linux.alibaba.com>
2023-02-22 10:54:57 +0800

    ioMonitor: Add ioMonitor to unity
    ioMonitor dependon unity, You must start ioMonitor after the unity is started
    and just as follow:
                sysak ioMonitor -y [plugin.yaml of unity]
    
    Signed-off-by: guangshui.li <guangshui.li@linux.alibaba.com>

---
+++ source/tools/monitor/ioMonitor/ioMon/exceptDiagnoseClass.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+
+import os
+import string
+import time
+from collections import OrderedDict
+import threading
+from tools.iofstool.iofsstat import iofsstatStart
+from tools.iowaitstat.iowaitstat import iowaitstatStart
+from displayClass import displayClass
+
+
+class runDiag(object):
+    def __init__(self, logRootPath, sender):
+        self.funcDicts = {
+            'iohang': self.startIohangDiagnose,
+            'ioutil': self.startIoutilDiagnose,
+            'iolatency': self.startIolatencyDiagnose,
+            'iowait': self.startIowaitDiagnose}
+        self.lastDiagTimeDicts = \
+            {'iohang': 0, 'ioutil': 0, 'iolatency': 0, 'iowait': 0}
+        self.display = displayClass(sender)
+        self.sysakPath = 'sysak'
+        self.logRootPath = logRootPath
+
+
+    def _recentDiagnoseValid(self, diagType):
+        return self.display.diagnoseValid(diagType)
+
+
+    def startIohangDiagnose(self, *argv):
+        devname = argv[0]
+        now = time.time()
+        if now - self.lastDiagTimeDicts['iohang'] <= 60:
+            return
+        startTime = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime(now))
+        logdir = self.logRootPath+'/iosdiag/hangdetect/'+startTime
+        file = logdir+'/result.log.seq'
+        outlog = logdir+'/resultCons.log'
+        if not os.path.exists(logdir):
+            try:
+                os.makedirs(logdir)
+            except Exception:
+                return
+        self.lastDiagTimeDicts['iohang'] = now
+        if devname is not None:
+            os.system(self.sysakPath+' -g iosdiag hangdetect -o -t 3000 -T 10 -f '+
+                      file+' '+devname+' > '+outlog+' &')
+        else:
+            os.system(self.sysakPath+' -g iosdiag hangdetect -o -t 3000 -T 10 -f '+
+                      file+' > '+outlog+' &')
+        self.display.start(20, 'iohang', logdir, now, now+60)
+
+
+    def startIolatencyDiagnose(self, *argv):
+        devname = argv[0]
+        thresh = argv[1]
+        ioburst = argv[2]
+        now = time.time()
+        if now - self.lastDiagTimeDicts['iolatency'] <= 60:
+            return
+        startTime = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime(now))
+        logdir = self.logRootPath+'/iosdiag/latency/'+startTime
+        file = logdir+'/result.log.seq'
+        outlog = logdir+'/resultCons.log'
+        if not os.path.exists(logdir):
+            try:
+                os.makedirs(logdir)
+            except Exception:
+                return
+        self.lastDiagTimeDicts['iolatency'] = now
+        if devname is not None:
+            os.system(self.sysakPath+' -g iosdiag latency -t '+str(thresh) +
+                      ' -T 45 -f '+file+' '+devname+' > '+outlog+' &')
+        else:
+            os.system(self.sysakPath+' -g iosdiag latency -t '+str(thresh) +
+                      ' -T 45 -f '+file+' > '+outlog+' &')
+        if ioburst:
+            self.display.markIoburst(now)
+        self.display.start(60, 'iolatency', logdir, now, now+60)
+
+
+    def startIoutilDiagnose(self, *argv):
+        devname = argv[0]
+        bwThresh = argv[1]
+        iopsThresh = argv[2]
+        now = time.time()
+        if now - self.lastDiagTimeDicts['ioutil'] <= 60:
+            return
+        startTime = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime(now))
+        logdir = self.logRootPath+'/iosdiag/iofsstat/'+startTime
+        outlog = logdir+'/resultCons.log'
+        if not os.path.exists(logdir):
+            try:
+                os.makedirs(logdir)
+            except Exception:
+                return
+        self.lastDiagTimeDicts['ioutil'] = now
+        #self.display.setIoburstThresh(iopsThresh, bwThresh)
+        argvs = ['-j',outlog,'-n','-m','-c','1','-t','5','-T','40',
+            '-i',str(iopsThresh),'-b',str(bwThresh)]
+        threading.Thread(target=iofsstatStart, args=(argvs,)).start()
+        self.display.start(55, 'ioutil', outlog, now, now+60)
+
+
+    def startIowaitDiagnose(self, *argv):
+        iowaitThresh = argv[0]
+        now = time.time()
+        if now - self.lastDiagTimeDicts['iowait'] <= 60:
+            return
+        startTime = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime(now))
+        logdir = self.logRootPath+'/iosdiag/iowaitstat/'+startTime
+        outlog = logdir+'/resultCons.log'
+        if not os.path.exists(logdir):
+            try:
+                os.makedirs(logdir)
+            except Exception:
+                return
+        self.lastDiagTimeDicts['iowait'] = now
+        argvs = ['-j', outlog, '-t', '5', '-w', str(iowaitThresh), '-T', '45']
+        threading.Thread(target=iowaitstatStart, args=(argvs,)).start()
+        self.display.start(55, 'iowait', outlog, now, now+60)
+
+
+    def runDiagnose(self, diagType, argv):
+        self.funcDicts[diagType](*list(argv))
+
+
+class diagnoseClass(runDiag):
+    def __init__(self, window, logRootPath, sender):
+        super(diagnoseClass, self).__init__(logRootPath, sender)
+        self.window = window
+        self.diagnoseDicts = OrderedDict()
+        self._diagStat = OrderedDict(
+                {'iohang': {'run': False, 'argv': [0, 0, 0, 0, 0, 0, 0, 0]},
+                'ioutil': {'run': False, 'argv': [0, 0, 0, 0, 0, 0, 0, 0]},
+                'iowait': {'run': False, 'argv': [0, 0, 0, 0, 0, 0, 0, 0]},
+                'iolatency': {'run': False, 'argv': [0, 0, 0, 0, 0, 0, 0, 0]}})
+
+
+    def addItem(self, devname, key, reportInterval, triggerInterval):
+        diagRecord = {
+            'statWindow': self.window,
+            'trigger': False,
+            'lastReport': 0,
+            'reportInterval': reportInterval,
+            'reportCnt': 0,
+            'lastDiag': 0,
+            'triggerInterval': triggerInterval,
+            'diagArgs': [0, 0, 0, 0, 0, 0, 0, 0]}
+        if devname not in self.diagnoseDicts.keys():
+            self.diagnoseDicts.setdefault(devname, {key: diagRecord})
+        else:
+            self.diagnoseDicts[devname].setdefault(key, diagRecord)
+
+
+    def setUpDiagnose(self, devname, key, nrSample, *argv):
+        diagnoseDicts = self.diagnoseDicts[devname][key]
+        lastDiag = diagnoseDicts['lastDiag']
+        lastReport = diagnoseDicts['lastReport']
+        statWindow = diagnoseDicts['statWindow']
+        reportInterval = diagnoseDicts['reportInterval']
+        triggerInterval = diagnoseDicts['triggerInterval']
+
+        if reportInterval != 0:
+            if lastReport == 0 or (nrSample-lastReport) > statWindow:
+                diagnoseDicts['lastReport'] = nrSample
+                diagnoseDicts['reportCnt'] = 1
+            else:
+                diagnoseDicts['reportCnt'] += 1
+            if diagnoseDicts['reportCnt'] > reportInterval:
+                if lastDiag == 0 or (nrSample-lastDiag) > triggerInterval:
+                    diagnoseDicts['trigger'] = True
+                    diagnoseDicts['reportCnt'] = 0
+                    diagnoseDicts['lastDiag'] = nrSample
+                else:
+                    diagnoseDicts['lastReport'] = nrSample
+                    diagnoseDicts['reportCnt'] = 0
+        elif triggerInterval != 0:
+            if lastDiag == 0 or (nrSample-lastDiag) >= triggerInterval:
+                diagnoseDicts['lastDiag'] = nrSample
+                diagnoseDicts['trigger'] = True
+        else:
+            diagnoseDicts['trigger'] = True
+
+        for idx, val in enumerate(argv):
+            diagnoseDicts['diagArgs'][idx] = val
+
+
+    def isException(self, devname, key):
+        diagnoseDicts = self.diagnoseDicts[devname][key]
+        reportInterval = diagnoseDicts['reportInterval']
+        triggerInterval = diagnoseDicts['triggerInterval']
+
+        if reportInterval != 0:
+            if (diagnoseDicts['reportCnt'] + 1) >= reportInterval:
+                return True
+        elif triggerInterval != 0:
+            return True
+        else:
+            return True
+        return False
+
+
+    def clearDiagStat(self):
+        for diagType, stat in self._diagStat.items():
+            stat['run'] = False
+            stat['argv'][0:] = [0, 0, 0, 0, 0, 0, 0, 0]
+
+
+    def checkDiagnose(self):
+        diagnoseDicts = self.diagnoseDicts
+        diagInfo = {'iohang': [], 'iolatency': [], 'ioutil': []}
+        diagStat = self._diagStat
+        ioburst = False
+
+        for devname, diagDict in diagnoseDicts.items():
+            if devname == 'system':
+                if diagDict['iowait']['trigger'] == True:
+                    diagStat['iowait']['run'] = True
+                    diagStat['iowait']['argv'][0] = \
+                        diagDict['iowait']['diagArgs'][0]
+                    diagDict['iowait']['trigger'] = False
+                continue
+
+            for diagType in ['iohang', 'iolatency', 'ioutil']:
+                if diagDict[diagType]['trigger'] == True:
+                    if diagType == 'iolatency':
+                        ioburst = diagDict['iolatency']['diagArgs'][1]
+                    diagInfo[diagType].append(devname)
+                    diagDict[diagType]['trigger'] = False
+
+        for diagType, value in diagInfo.items():
+            diagStat[diagType]['run'] = True
+            if len(value) > 1:
+                diagStat[diagType]['argv'][0] = None
+            elif len(value) == 1:
+                diagStat[diagType]['argv'][0] = value[0]
+            else:
+                diagStat[diagType]['run'] = False
+
+        if diagStat['ioutil']['run'] == True:
+            for idx in [1,2]:
+                val = sorted(
+                    [diagnoseDicts[dev]['ioutil']['diagArgs'][idx-1] 
+                    for dev in diagInfo['ioutil']],
+                    reverse=True)
+                diagStat['ioutil']['argv'][idx] = val[-1]
+
+        if diagStat['iolatency']['run'] == True:
+            diagStat['iolatency']['argv'][1] = sorted(
+                [diagnoseDicts[dev]['iolatency']['diagArgs'][0] 
+                for dev in diagInfo['iolatency']],
+                reverse=True)[-1]
+            diagStat['iolatency']['argv'][2] = ioburst
+
+        for diagType, stat in diagStat.items():
+            if stat['run'] == True:
+                self.runDiagnose(diagType, stat['argv'])
+                stat['run'] = False
+
+
+    # In displayClass, after the diagnostic log is reported to the remote end, 
+    # it will be marked as a valid diagnosis, and In exceptDiagnoseClass, 
+    # clear the valid mark before each diagnosis
+    def recentDiagnoseValid(self, diagType):
+        return self._recentDiagnoseValid(diagType)
---
+++ source/tools/monitor/ioMonitor/ioMon/ioMonitorClass.py
@@ -0,0 +1,369 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import signal
+import string
+import argparse
+import time
+from exceptDiagnoseClass import diagnoseClass
+from exceptCheckClass import exceptCheckClass
+from ioMonCfgClass import ioMonCfgClass
+from collections import OrderedDict
+from nfPut import CnfPut
+
+class ioMonitorClass(object):
+    def __init__(self, logRootPath, cfg, pipeFile):
+        self.window = 60
+        self.cfg = cfg
+        self.cfg.createCfgFlagFile()
+        self.diagSwitch = {
+            'diagIowait': {'sw': self.cfg.getCfgItem('diagIowait'),
+                            'esi':'IOwait-High'},
+            'diagIoburst': {'sw': self.cfg.getCfgItem('diagIoburst'),
+                            'esi':'IO-Delay'},
+            'diagIolat': {'sw': self.cfg.getCfgItem('diagIolat'),
+                            'esi':'IO-Burst'},
+            'diagIohang': {'sw': self.cfg.getCfgItem('diagIohang'),
+                            'esi':'IO-Hang'}
+            }
+        self._sender = CnfPut(pipeFile)
+        self._nfPutTlb = 'IOMonIndForDisksIO'
+        self._nfPutTlb4System = 'IOMonIndForSystemIO'
+        self.fieldDicts = OrderedDict()
+        self.exceptChkDicts = {'system': exceptCheckClass(self.window)}
+        self.exceptChkDicts['system'].addItem('iowait')
+        self.diagnose = diagnoseClass(self.window, logRootPath, self._sender)
+        self.diagnose.addItem('system', 'iowait', 0, 60)
+        self.fDiskStats = open("/proc/diskstats")
+        self.cpuStatIowait = {'sum': 0, 'iowait': 0}
+        self.uploadInter = 0
+        self.exceptionStat = {'system': {'IOwait-High': {'cur':0,'max':0}}}
+        self.dataStat = {'system': {'iowait': 0}}
+
+
+    def _addMonitorAttrForDisk(self, disk):
+        dataStat = self.dataStat
+        exceptChkDicts = self.exceptChkDicts
+        diagnose = self.diagnose
+        exceptionStat = self.exceptionStat
+
+        # used for reporting per-index to database
+        dataStat.setdefault(
+            disk, {'await': 0, 'util': 0, 'iops': 0, 'bps': 0, 'qusize': 0})
+
+        # add exception-check attr for per-index on per-disk
+        exceptChkDicts.setdefault(disk, exceptCheckClass(self.window))
+        for key in ['util', 'await', 'iops', 'bps']:
+            exceptChkDicts[disk].addItem(key)
+
+        # add diagnose attr for per-index on per-disk
+        diagnoseDict = {
+            'iohang': {'triggerInterval': self.window * 5,
+                        'reportInterval': 10},
+            'ioutil': {'triggerInterval': 60, 'reportInterval': 0},
+            'iolatency': {'triggerInterval': 60, 'reportInterval': 0}
+            }
+        for key, item in diagnoseDict.items():
+            diagnose.addItem(
+                disk, key, item['reportInterval'], item['triggerInterval'])
+            # used for reporting exception to database
+            exceptionStat.setdefault(
+                disk,
+                {'IO-Delay':{'cur':0,'max':0}, 'IO-Burst':{'cur':0,'max':0},
+                'IO-Hang':{'cur':0,'max':0}})
+
+
+    def _removeDiskMonitor(self, disk):
+        del self.fieldDicts[disk]
+        del self.dataStat[disk]
+        del self.exceptChkDicts[disk]
+        del self.diagnose[disk]
+        del self.exceptionStat[disk]
+
+
+    def _disableThreshComp(self, devname, qusize):
+        exceptChkDicts = self.exceptChkDicts
+        exceptChkDicts[devname].disableThreshComp('util')
+        exceptChkDicts[devname].disableThreshComp('iops')
+        exceptChkDicts[devname].disableThreshComp('bps')
+        if qusize > 1:
+            exceptChkDicts[devname].disableThreshComp('await')
+
+
+    def _calcIowait(self):
+        with open("/proc/stat") as fStat:
+            statList = map(long, fStat.readline().split()[1:])
+        iowait = float(format(
+            (statList[4] - self.cpuStatIowait['iowait']) * 100.0 /
+            (sum(statList) - self.cpuStatIowait['sum']), '.2f'))
+        return iowait
+    
+
+    def _calcIoIndex(self, devname, field, secs):
+        ds = self.dataStat[devname]
+        uploadInter = self.uploadInter
+
+        rws = field['1'][1] + field['5'][1] - field['1'][0] - field['5'][0]
+        iops = round(rws / secs, 1)
+        ds['iops'] = (ds['iops'] * (uploadInter - 1) + iops) / uploadInter
+
+        rwSecs = field['3'][1] + field['7'][1] - field['3'][0] - field['7'][0]
+        bps = rwSecs / secs * 512
+        ds['bps'] = (ds['bps'] * (uploadInter - 1) + bps) / uploadInter
+
+        qusize = round((field['11'][1] - field['11'][0]) / secs / 1000, 2)
+        ds['qusize'] = (ds['qusize'] * (uploadInter - 1) + qusize) / uploadInter
+
+        rwTiks = field['4'][1] + field['8'][1] - field['4'][0] - field['8'][0]
+        wait = round(rwTiks / (iops * secs), 2) if iops else 0
+        ds['await'] = (ds['await'] * (uploadInter - 1) + wait) / uploadInter
+
+        times = field['10'][1] - field['10'][0]
+        util = round(times * 100.0 / (secs * 1000), 2)
+        util = util if util <= 100 else 100.0
+        ds['util'] = (ds['util'] * (uploadInter - 1) + util) / uploadInter
+
+        return {'iops': iops*secs, 'bps': bps*secs, 'qusize': qusize*secs,
+            'wait': wait, 'util': util}
+
+
+    def _checkDiagSwitchChange(self, t):
+        s = self.diagSwitch[t]
+        newSW = self.cfg.getCfgItem(t)
+        if newSW != s['sw'] and newSW == 'on':
+            for k,v in self.exceptionStat.items():
+                if s['esi'] in v.keys():
+                    v[s['esi']]['max'] = 0
+        s['sw'] = newSW
+        return newSW
+
+
+    def _checkIOwaitException(self, iowait):
+        exceptChk = self.exceptChkDicts['system']
+        dataStat = self.dataStat['system']
+        es = self.exceptionStat['system']['IOwait-High']
+        diagnose = self.diagnose
+        uploadInter = self.uploadInter
+
+        if iowait >= self.cfg.getCfgItem('iowait'):
+            exceptChk.disableThreshComp('iowait')
+
+        dataStat['iowait'] = \
+            (dataStat['iowait'] * (uploadInter - 1) + iowait) / uploadInter
+
+        # Detect iowait exception
+        minThresh = self.cfg.getCfgItem('iowait')
+        iowaitThresh = max(exceptChk.getDynThresh('iowait'), minThresh)
+        if iowait >= iowaitThresh:
+            es['cur'] += 1
+            diagSW = self._checkDiagSwitchChange('diagIowait')
+            rDiagValid = diagnose.recentDiagnoseValid('iowait')
+            # Configure iowait diagnosis
+            if diagSW == 'on' and (es['cur'] > es['max'] or not rDiagValid):
+                nrSample = exceptChk.getNrDataSample('iowait')
+                iowaitArg = max(int(iowait * 0.25), minThresh)
+                diagnose.setUpDiagnose('system', 'iowait', nrSample, iowaitArg)
+
+        exceptChk.updateDynThresh('iowait', iowait)
+
+    def _checkIoburstException(self, devname, es, bps, iops, exceptChk):
+        bpsLowW = self.cfg.getCfgItem('bps')
+        bpsHighW = max(exceptChk.getDynThresh('bps'), bpsLowW)
+        bpsMiddW = max(bpsLowW, bpsHighW / 2)
+        iopsLowW = self.cfg.getCfgItem('iops')
+        iopsHighW = max(exceptChk.getDynThresh('iops'), iopsLowW)
+        iopsMiddW = max(iopsLowW, iopsHighW / 2)
+        ioburst = exception = False
+
+        if iops >= iopsMiddW or bps >= bpsMiddW:
+            ioburst = True
+        bpsOver = True if bps >= bpsHighW else False
+        iopsOver = True if iops >= iopsHighW else False
+
+        if iopsOver or bpsOver:
+            es['cur'] += 1
+            diagSW = self._checkDiagSwitchChange('diagIoburst')
+            rDiagValid = self.diagnose.recentDiagnoseValid('ioutil')
+            # Configure IO load diagnosis
+            if diagSW == 'on' and (es['cur'] > es['max'] or not rDiagValid):
+                bpsArg = iopsArg = 0
+                if bpsOver == True:
+                    bpsArg = max(int(bps * 0.25), bpsLowW)
+                if iopsOver == True:
+                    iopsArg = max(int(iops * 0.7), iopsLowW)
+                nrSample = exceptChk.getNrDataSample('util')
+                self.diagnose.setUpDiagnose(
+                    devname, 'ioutil', nrSample, bpsArg, iopsArg)
+        return ioburst
+
+    def _checkIohangException(
+        self, devname, es, util, qusize, iops, exceptChk):
+        # Detect IO hang
+        if util >= 100 and qusize >= 1 and iops < 50:
+            # Configure IO hang diagnosis
+            if self.diagnose.isException(devname, 'iohang') == True:
+                es['cur'] += 1
+            diagSW = self._checkDiagSwitchChange('diagIohang')
+            rDiagValid = self.diagnose.recentDiagnoseValid('iohang')
+            if diagSW == 'on' and (es['cur'] > es['max'] or not rDiagValid):
+                nrSample = exceptChk.getNrDataSample('util')
+                self.diagnose.setUpDiagnose(devname, 'iohang', nrSample)
+
+    def _checkUtilException(self, devname, util, iops, bps, qusize):
+        exceptChk = self.exceptChkDicts[devname]
+        exceptionStat = self.exceptionStat[devname]
+        diagnose = self.diagnose
+        ioburst = False
+
+        utilMinThresh = self.cfg.getCfgItem('util')
+        utilThresh = max(exceptChk.getDynThresh('util'), utilMinThresh)
+        if util >= utilThresh:
+            es = exceptionStat['IO-Burst']
+            ioburst = \
+                self._checkIoburstException(devname, es, bps, iops, exceptChk)
+            if not ioburst:
+                es = exceptionStat['IO-Hang']
+                self._checkIohangException(
+                    devname, es, util, qusize, iops, exceptChk)                 
+        exceptChk.updateDynThresh('util', util)
+        exceptChk.updateDynThresh('iops', iops)
+        exceptChk.updateDynThresh('bps', bps)
+        return ioburst
+
+
+    def _checkAwaitException(self, devname, wait, ioburst):
+        exceptChk = self.exceptChkDicts[devname]
+        es = self.exceptionStat[devname]['IO-Delay']
+        diagnose = self.diagnose
+
+        awaitMinThresh = self.cfg.getCfgItem('await')
+        awaitThresh = max(exceptChk.getDynThresh('await'), awaitMinThresh)
+        if wait >= awaitThresh:
+            es['cur'] += 1
+            diagSW = self._checkDiagSwitchChange('diagIolat')
+            rDiagValid = diagnose.recentDiagnoseValid('iolatency')
+            # Configuring IO delay diagnostics
+            if diagSW == 'on' and (es['cur'] > es['max'] or not rDiagValid):
+                nrSample = exceptChk.getNrDataSample('await')
+                waitArg = max(int(wait * 0.4), awaitMinThresh)
+                diagnose.setUpDiagnose(
+                    devname, 'iolatency', nrSample, waitArg, ioburst)
+        exceptChk.updateDynThresh('await', wait)
+
+
+    def _reportDataToRemote(self, devList):
+        # report datastat&&exception to database
+        nCycle = 1000.0 / float(self.cfg.getCfgItem('cycle'))
+        dataStat = self.dataStat['system']
+        es = self.exceptionStat['system']['IOwait-High']
+
+        putIdx = ',idx_type=system_Indicator,devname=system '
+        putField = 'iowait=%f,iowaithighCnt=%f' %(
+            dataStat['iowait'], es['cur'] / nCycle)
+        self._sender.puts(self._nfPutTlb4System + putIdx + putField)
+
+        es['max'] = max(es['max'] if es['cur'] else 0, es['cur'])
+        es['cur'] = 0
+        cur = {'IO-Delay':0, 'IO-Burst':0, 'IO-Hang':0}
+        for devname in devList:
+            dataStat = self.dataStat[devname]
+            es = self.exceptionStat[devname]
+            for type in cur.keys():
+                cur[type] = int(es[type]['cur']) / nCycle
+                es[type]['max'] = \
+                    max(es[type]['max'] if cur[type] else 0, cur[type])
+                es[type]['cur'] = 0
+
+            putIdx = ',idx_type=iostat_Indicator,devname=%s ' % devname
+            putField = 'await=%f,util=%f,iops=%f,bps=%f,qusize=%f' %(
+                dataStat['await'], dataStat['util'], dataStat['iops'],
+                dataStat['bps'] / 1024.0, dataStat['qusize']
+            )
+            putField += ',iodelayCnt=%f,ioburstCnt=%f,iohangCnt=%f' %(
+                cur['IO-Delay'], cur['IO-Burst'], cur['IO-Hang'])
+            self._sender.puts(self._nfPutTlb + putIdx + putField)
+
+
+    def _collectBegin(self):
+        fieldDicts = self.fieldDicts
+
+        # collect iowait begin
+        with open("/proc/stat") as fStat:
+            cpuStatList = map(long, fStat.readline().split()[1:])
+            self.cpuStatIowait['sum'] = sum(cpuStatList)
+            self.cpuStatIowait['iowait'] = cpuStatList[4]
+        
+        # collect iostat begin
+        self.fDiskStats.seek(0)
+        for stat in self.fDiskStats.readlines():
+            stat = stat.split()
+            if os.path.exists('/sys/block/'+stat[2]) == False:
+                if stat[2] in fieldDicts.keys():
+                    self._removeDiskMonitor(stat[2])
+                continue
+
+            if stat[2] in fieldDicts.keys():
+                field = fieldDicts[stat[2]]
+            else:
+                field = {
+                    '1': [0, 0], '3': [0, 0], '4': [0, 0],
+                    '5': [0, 0], '7': [0, 0], '8': [0, 0],
+                    '10': [0, 0], '11': [0, 0]}
+                 # add data staticsis for per-disk
+                fieldDicts.setdefault(stat[2], field)
+                self._addMonitorAttrForDisk(stat[2])
+
+            for idx, value in field.items():
+                value[0] = long(stat[int(idx) + 2])
+
+
+    def _collectEnd(self, secs):
+        fieldDicts = self.fieldDicts
+        exceptChkDicts = self.exceptChkDicts
+        uploadInter = self.uploadInter
+        
+        self.uploadInter = \
+            1 if ((uploadInter * secs) % 60) == 0 else (uploadInter + 1)
+
+        # Calculate iowait
+        iowait = self._calcIowait()
+        # Detect iowait exception
+        self._checkIOwaitException(iowait)
+
+        # collect iostat end
+        self.fDiskStats.seek(0)
+        for stat in self.fDiskStats.readlines():
+            stat = stat.split()
+            if os.path.exists('/sys/block/'+stat[2]) == False:
+                if stat[2] in fieldDicts.keys():
+                    self._removeDiskMonitor(stat[2])
+                continue
+            for idx, value in fieldDicts[stat[2]].items():
+                value[1] = long(stat[int(idx) + 2])
+
+        for devname, field in fieldDicts.items():
+            io = self._calcIoIndex(devname, field, secs)
+            if io['util'] >= self.cfg.getCfgItem('util'):
+                # There is IO Burst at present, turn off threshold compensation
+                self._disableThreshComp(devname, io['qusize'])
+            # Detect util exception
+            ioburst = self._checkUtilException(
+                devname, io['util'], io['iops'], io['bps'], io['qusize'])
+            # Detect await exception
+            self._checkAwaitException(devname, io['wait'], ioburst)
+
+        if ((self.uploadInter * secs) % 60) == 0:
+            self._reportDataToRemote(fieldDicts.keys())
+
+
+    def monitor(self):
+        while True:
+            secs = self.cfg.getCfgItem('cycle') / 1000.0
+            self._collectBegin()
+            time.sleep(secs)
+            self._collectEnd(secs)
+            # Check if it is necessary to start the diagnosis
+            self.diagnose.checkDiagnose()
+        self.fDiskStats.close()
+
