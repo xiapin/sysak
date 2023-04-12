c225f2b0d86175210b750517d8c65d4a6af215a2
Hailong Liu <liuhailong@linux.alibaba.com>
2023-04-12 16:17:57 +0800

    pmu_events: Merge LLC miss and hw events to a group
    This can have a little performance improvement.
    
    Signed-off-by: Hailong Liu <liuhailong@linux.alibaba.com>

--- source/tools/monitor/unity/collector/plugin/pmu_events/pmu_events.c
+++ source/tools/monitor/unity/collector/plugin/pmu_events/pmu_events.c
@@ -10,8 +10,8 @@ double summary[NR_EVENTS];
 struct pcpu_hw_info *gpcpu_hwi;
 struct pmu_events *glb_pme;
 char *events_str[] = {"cycles", "ins", "refCyc",
-			"llcLoad", "llcLoadMis",
-			"llcStore", "llcStoreMis"};
+			"llcLoadMis", "llcStoreMis"
+			"llcLoad", "llcStore"};
 char *value_str[] = {"cycles", "instructions", "CPI",
 			"llc_load_ref", "llc_load_miss", "LLC_LMISS_RATE"
 			"llc_store_ref", "llc_store_miss", "LLC_SMIRSS_RATE"};
