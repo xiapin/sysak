30d34707458a3e001211c892d39d4782cd0968c2
Hailong Liu <liuhailong@linux.alibaba.com>
2023-05-10 11:39:37 +0800

    add test
    Signed-off-by: Hailong Liu <liuhailong@linux.alibaba.com>

---
+++ source/tools/test/bpf_so_test/bpf/bpftest2.bpf.c
@@ -0,0 +1,19 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include "../bpftest.h"
+
+
+
+SEC("kprobe/tcp_v4_connect")
+int BPF_KPROBE(kprobe_tcp_v4_connect,struct sock *sk, struct sockaddr *uaddr, int addr_len)
+{
+    u64 tgid = bpf_get_current_pid_tgid();
+    int tmp;
+    BPF_CORE_READ_INTO(&tmp, sk, sk_rcvbuf);
+    return 0;
+}
+
+
+char LICENSE[] SEC("license") = "GPL";
