0d40e60f9ccdd2ca1f9ac8d84e4a2a602579ec57
liaozhaoyan <zhaoyan.liao@linux.alibaba.com>
2023-06-04 20:41:59 +0800

    change oss data format to octet-stream, last used json.

--- source/tools/monitor/unity/test/curl/postOSS.py
+++ source/tools/monitor/unity/test/curl/postOSS.py
@@ -4,6 +4,8 @@ import uuid
 import json
 
 url = "http://127.0.0.1:8400/api/oss"
-d = {"stream": "hello oss", "uuid": str(uuid.uuid4())}
-res = requests.post(url, json=d)
-print(res)
+headers = {'Content-Type': 'application/octet-stream', 'uuid': str(uuid.uuid4())}
+data = b'binary data'
+
+response = requests.post(url, headers=headers, data=data)
+print(response)
