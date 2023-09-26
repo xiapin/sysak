08651aa78b2be95140f628ee25a95c760ea1573b
xiaozhenwei <xiaozhenwei.xzw@alibaba-inc.com>
2023-09-26 17:28:08 +0800

    add: Notify nopto of the mysqld pid

--- source/tools/monitor/observ/sql-obs/tracing/sqlTracing.go
+++ source/tools/monitor/observ/sql-obs/tracing/sqlTracing.go
@@ -1,9 +1,35 @@
 package tracing
 
 import (
-    //"fmt"
+	"errors"
+	"fmt"
+	"net"
+	"os"
 )
 
-func StartTracingSql() {
-	//fmt.Println("start tracing sql requests")
+func notifyMysqlInstancesInfo(pidList string) error {
+	if _, err := os.Stat("/var/ntopo"); os.IsNotExist(err) {
+		return err
+	}
+	addr, err := net.ResolveUnixAddr("unix", "/var/ntopo")
+	if err != nil {
+		return err
+	}
+	sock, err := net.DialUnix("unix", nil, addr)
+	// gCnfPut.sock, err = net.Dial("unixgram", path)
+	if err != nil {
+		return err
+	}
+
+	if len(pidList) > MAX_BUFF {
+		return errors.New("pidList too long")
+	}
+
+	if _, err := sock.Write([]byte(pidList)); err != nil {
+		//fmt.Println(s)
+		fmt.Printf("output: %s\n", pidList)
+		return err
+	}
+
+	return nil
 }
