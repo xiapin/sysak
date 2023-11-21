36ad52a76bc6b39d306980dff68dc26632f2f7a3
六滔 <xiaojietao.xjt@alibaba-inc.com>
2023-11-21 11:33:54 +0800

    unity/container: fix container network interfaces bug

--- source/tools/monitor/unity/collector/container/con_net_stat.lua
+++ source/tools/monitor/unity/collector/container/con_net_stat.lua
@@ -43,41 +43,33 @@ local function isIgnoredDevice(devname)
     return false
 end
 
-local function copyTable(original)
-    local copy = {}
-    for key, value in pairs(original) do
-        copy[key] = value
-    end
-    return copy
-end
-
 function ConNetStat:proc(elapsed, lines)
     local c = 1
     local k = 1
-    local local_ls = copyTable(self.ls)
     local devName
     local RxBytes, RxPackets = 0, 0
     local TxBytes, TxPackets = 0, 0
+    local RxPacketsDrop, TxPacketsDrop = 0, 0
     local values = {}
     CvProc.proc(self)
     self:_getProcNetPath_()
-    table.insert(local_ls, {name = "interface", index = ""})
 
     for line in io.lines(self.procnetpath) do
         local cell
+        local new_line = line
 
         --- skip first two lines
         if c < 3 then
             c = c + 1
             goto continue
         end
-    
-        line = pystring:replace(line, ":", "")
+
+        new_line = pystring:replace(new_line, ":", "")
         --- "eth0" and "lo" may start with " "
-        line = pystring:lstrip(line)
-        cell = pystring:split(line)
+        new_line = pystring:lstrip(new_line)
+        cell = pystring:split(new_line)
         if #cell ~= 17 then
-            print("invalid interface stats line " .. line)
+            print("invalid interface stats line " .. new_line)
             goto continue
         end
 
@@ -87,50 +79,42 @@ function ConNetStat:proc(elapsed, lines)
             goto continue
         end
 
-        RxBytes = tonumber(cell[2])
-        RxPackets = tonumber(cell[3])
-        RxPacketsDrop = tonumber(cell[5])
-        TxBytes = tonumber(cell[10])
-        TxPackets = tonumber(cell[11])
-        TxPacketsDrop = tonumber(cell[13])
-
-        -- change "device" label to certain dev 
-        for _, item in ipairs(local_ls) do 
-            if item.name == "interface" then
-                item.index = devName
-                break
-            end
-        end
-        
-        values[k] = {
-            name = "net_rx_bytes",
-            value = RxBytes
-        }
-        values[k + 1] = {
-            name = "net_rx_packets",
-            value = RxPackets
-        }
-        values[k + 2] = {
-            name = "net_rx_dropped",
-            value = RxPacketsDrop
-        }
-        values[k + 3] = {
-            name = "net_tx_bytes",
-            value = TxBytes
-        }
-        values[k + 4] = {
-            name = "net_tx_packets",
-            value = TxPackets
-        }
-        values[k + 5] = {
-            name = "net_tx_dropped",
-            value = TxPacketsDrop
-        }
-
-        self:appendLine(self:_packProto("con_net_stat", local_ls , values))
+        RxBytes = RxBytes + tonumber(cell[2])
+        RxPackets = RxPackets + tonumber(cell[3])
+        RxPacketsDrop = RxPacketsDrop + tonumber(cell[5])
+        TxBytes = TxBytes + tonumber(cell[10])
+        TxPackets = TxPackets + tonumber(cell[11])
+        TxPacketsDrop = TxPacketsDrop + tonumber(cell[13])
 
         ::continue::
-    end    
+    end
+
+    values[k] = {
+        name = "net_rx_bytes",
+        value = RxBytes
+    }
+    values[k + 1] = {
+        name = "net_rx_packets",
+        value = RxPackets
+    }
+    values[k + 2] = {
+        name = "net_rx_dropped",
+        value = RxPacketsDrop
+    }
+    values[k + 3] = {
+        name = "net_tx_bytes",
+        value = TxBytes
+    }
+    values[k + 4] = {
+        name = "net_tx_packets",
+        value = TxPackets
+    }
+    values[k + 5] = {
+        name = "net_tx_dropped",
+        value = TxPacketsDrop
+    }
+
+    self:appendLine(self:_packProto("con_net_stat", self.ls, values))
     self:push(lines)
 end
 
